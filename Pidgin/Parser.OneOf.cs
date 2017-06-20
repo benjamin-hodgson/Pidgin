995a66a9cf2eb6cf3014f44aa123f801eb10c223
Benjamin Hodgson <bhodgson@stackoverflow.com>
2017-06-20 14:32:59 +0100

    ~5x performance boost for heavily nested Or calls, by flattening the list

--- Pidgin/Parser.OneOf.cs
+++ Pidgin/Parser.OneOf.cs
@@ -43,14 +43,14 @@ namespace Pidgin
         /// <param name="parsers">A sequence of parsers to choose between</param>
         /// <returns>A parser which applies one of the specified parsers</returns>
         public static Parser<TToken, T> OneOf<TToken, T>(IEnumerable<Parser<TToken, T>> parsers)
-            => new OneOfParser<TToken, T>(parsers.ToList());
+            => OneOfParser<TToken, T>.Create(parsers.ToList());
 
 
         private sealed class OneOfParser<TToken, T> : Parser<TToken, T>
         {
             private readonly List<Parser<TToken, T>> _parsers;
 
-            public OneOfParser(List<Parser<TToken, T>> parsers)
+            private OneOfParser(List<Parser<TToken, T>> parsers)
                 : base(ExpectedUtil.Union(parsers.Select(p => p.Expected)))
             {
                 _parsers = parsers;
@@ -76,6 +76,28 @@ namespace Pidgin
 
             private static Result<TToken, T> LongestMatch(Result<TToken, T> result1, Result<TToken, T> result2)
                 => result1.Error.ErrorPos > result2.Error.ErrorPos ? result1 : result2;
+
+            internal static OneOfParser<TToken, T> Create(IEnumerable<Parser<TToken, T>> parsers)
+            {
+                // if we know the length of the collection,
+                // we know we're going to need at least that much room in the list
+                var list = parsers is ICollection<Parser<TToken, T>> coll
+                    ? new List<Parser<TToken, T>>(coll.Count)
+                    : new List<Parser<TToken, T>>();
+                
+                foreach (var p in parsers)
+                {
+                    if (p is OneOfParser<TToken, T> o)
+                    {
+                        list.AddRange(o._parsers);
+                    }
+                    else
+                    {
+                        list.Add(p);
+                    }
+                }
+                return new OneOfParser<TToken, T>(list);
+            }
         }
     }
 }
