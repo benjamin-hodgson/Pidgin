231c752c71359cb29bf150d60cc406b3cb75dbcb
Benjamin Hodgson <bhodgson@stackoverflow.com>
2017-10-14 01:24:35 +0100

    Added note to docs for Not

--- Pidgin/Parser.Not.cs
+++ Pidgin/Parser.Not.cs
@@ -10,6 +10,7 @@ namespace Pidgin
         /// <summary>
         /// Creates a parser which succeeds only if the given parser fails.
         /// The resulting parser does not perform any backtracking; it consumes the same amount of input as the supplied parser.
+        /// Combine this function with <see cref="Parser.Try{TToken, T}(Parser{TToken, T})"/> if this behaviour is undesirable.
         /// </summary>
         /// <param name="parser">The parser that is expected to fail</param>
         /// <returns>A parser which succeeds only if the given parser fails.</returns>
