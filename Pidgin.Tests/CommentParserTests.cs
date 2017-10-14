6e44af6825bb82009d83e876637b9160814c806a
Benjamin Hodgson <bhodgson@stackoverflow.com>
2017-10-14 02:09:54 +0100

    Wrote CommentParser

---
+++ Pidgin.Tests/CommentParserTests.cs
@@ -0,0 +1,81 @@
+using Pidgin.Comment;
+using Xunit;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace Pidgin.Tests
+{
+    public class CommentParserTests : ParserTestBase
+    {
+        [Fact]
+        public void TestSkipLineComment()
+        {
+            var p = CommentParser.SkipLineComment(String("//")).Then(End());
+
+            {
+                var comment = "// here is a comment ending with an osx style newline\n";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+            {
+                var comment = "// here is a comment ending with a windows style newline\r\n";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+            {
+                var comment = "// here is a comment with a \r carriage return in the middle\r\n";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+            {
+                var comment = "// here is a comment at the end of a file";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+        }
+
+        [Fact]
+        public void TestSkipBlockComment()
+        {
+            var p = CommentParser.SkipBlockComment(String("/*"), String("*/")).Then(End());
+
+            {
+                var comment = "/* here is a block comment with \n newlines in */";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+        }
+
+        [Fact]
+        public void TestSkipNestedBlockComment()
+        {
+            var p = CommentParser.SkipNestedBlockComment(String("/*"), String("*/")).Then(End());
+            
+            {
+                var comment = "/* here is a non-nested block comment with \n newlines in */";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+            
+            {
+                var comment = "/* here is a /* nested */ block comment with \n newlines in */";
+
+                var result = p.Parse(comment);
+
+                AssertSuccess(result, Unit.Value, true);
+            }
+        }
+    }
+}
---
+++ Pidgin/Comment/CommentParser.cs
@@ -0,0 +1,85 @@
+using System;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace Pidgin.Comment
+{
+    /// <summary>
+    /// Contains functions to build parsers which skip over comments
+    /// </summary>
+    public static class CommentParser
+    {
+        /// <summary>
+        /// Creates a parser which runs <paramref name="lineCommentStart"/>, then skips the rest of the line.
+        /// </summary>
+        /// <param name="lineCommentStart">A parser to recognise a lexeme which starts a line comment</param>
+        /// <returns>A parser which runs <paramref name="lineCommentStart"/>, then skips the rest of the line.</returns>
+        public static Parser<char, Unit> SkipLineComment<T>(Parser<char, T> lineCommentStart)
+        {
+            if (lineCommentStart == null)
+            {
+                throw new ArgumentNullException(nameof(lineCommentStart));
+            }
+
+            var eol = Try(EndOfLine).IgnoreResult();
+            return lineCommentStart
+                .Then(Any.SkipUntil(End().Or(eol)))
+                .Labelled("line comment");
+        }
+
+        /// <summary>
+        /// Creates a parser which runs <paramref name="blockCommentStart"/>,
+        /// then skips everything until <paramref name="blockCommentEnd"/>.
+        /// </summary>
+        /// <param name="blockCommentStart">A parser to recognise a lexeme which starts a multi-line block comment</param>
+        /// <param name="blockCommentEnd">A parser to recognise a lexeme which ends a multi-line block comment</param>
+        /// <returns>
+        /// A parser which runs <paramref name="blockCommentStart"/>, then skips everything until <paramref name="blockCommentEnd"/>.
+        /// </returns>
+        public static Parser<char, Unit> SkipBlockComment<T, U>(Parser<char, T> blockCommentStart, Parser<char, U> blockCommentEnd)
+        {
+            if (blockCommentStart == null)
+            {
+                throw new ArgumentNullException(nameof(blockCommentStart));
+            }
+            if (blockCommentEnd == null)
+            {
+                throw new ArgumentNullException(nameof(blockCommentEnd));
+            }
+
+            return blockCommentStart
+                .Then(Any.SkipUntil(blockCommentEnd))
+                .Labelled("block comment");
+        }
+
+        /// <summary>
+        /// Creates a parser which runs <paramref name="blockCommentStart"/>,
+        /// then skips everything until <paramref name="blockCommentEnd"/>, accounting for nested comments.
+        /// </summary>
+        /// <param name="blockCommentStart">A parser to recognise a lexeme which starts a multi-line block comment</param>
+        /// <param name="blockCommentEnd">A parser to recognise a lexeme which ends a multi-line block comment</param>
+        /// <returns>
+        /// A parser which runs <paramref name="blockCommentStart"/>,
+        /// then skips everything until <paramref name="blockCommentEnd"/>, accounting for nested comments.
+        /// </returns>
+        public static Parser<char, Unit> SkipNestedBlockComment<T, U>(Parser<char, T> blockCommentStart, Parser<char, U> blockCommentEnd)
+        {
+            if (blockCommentStart == null)
+            {
+                throw new ArgumentNullException(nameof(blockCommentStart));
+            }
+            if (blockCommentEnd == null)
+            {
+                throw new ArgumentNullException(nameof(blockCommentEnd));
+            }
+
+            Parser<char, Unit> parser = null;
+
+            parser = blockCommentStart.Then(
+                Rec(() => parser).Or(Any.IgnoreResult()).SkipUntil(blockCommentEnd)
+            ).Labelled("block comment");
+
+            return parser;
+        }
+    }
+}
