281203cf8dcbe0f478bb278e9b7f9e0d2ac362ea
Benjamin Hodgson <bhodgson@stackoverflow.com>
2017-11-01 18:00:54 +0000

    Added Trace and TraceResult methods for debugging; bumped netstandard dependency to 1.3 (to get Console)

---
+++ Pidgin/Parser.Trace.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Pidgin
+{
+    public partial class Parser<TToken, T>
+    {
+        /// <summary>
+        /// For debugging use.
+        /// 
+        /// Creates a new parser which runs the current parser and prints the given message to the console.
+        /// </summary>
+        /// <returns>A parser which runs the current parser and prints the given message to the console.</returns>
+        public Parser<TToken, T> Trace(Func<T, string> message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            return this.Select(x =>
+            {
+                Console.WriteLine(message(x));
+                return x;
+            });
+        }
+
+        /// <summary>
+        /// For debugging use.
+        /// 
+        /// Creates a new parser which runs the current parser and prints the given message to the console.
+        /// </summary>
+        /// <returns>A parser which runs the current parser and prints the given message to the console.</returns>
+        public Parser<TToken, T> Trace(string message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            return this.Trace(_ => message);
+        }
+
+        /// <summary>
+        /// For debugging use.
+        /// 
+        /// Creates a new parser which runs the current parser and prints the result to the console.
+        /// </summary>
+        /// <returns>A parser which runs the current parser and prints the result to the console.</returns>
+        public Parser<TToken, T> TraceResult() => this.Trace(x => x.ToString());
+    }
+}
