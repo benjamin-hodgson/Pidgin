3898406c6ce89f17e701ff73e359444627f9094b
Benjamin Hodgson <bhodgson@stackoverflow.com>
2018-08-10 17:22:17 +0100

    target for netcoreapp; more efficient code for AsRef, per original

--- Pidgin.DllRewriter/Program.cs
+++ Pidgin.DllRewriter/Program.cs
@@ -10,16 +10,18 @@ namespace Pidgin.DllRewriter
     {
         static void Main(string[] args)
         {
-            var module = ModuleDefinition.ReadModule(args[0]);
+            var netcoreapp = args[0].StartsWith("netcoreapp");
+
+            var module = ModuleDefinition.ReadModule(args[1]);
 
 
 
             var unsafeType = module.Types.Single(t => t.Name == "Unsafe");
 
             RewriteAsPointerMethod(unsafeType);
-            RewriteAsRefMethod(unsafeType, module.TypeSystem);
+            RewriteAsRefMethod(netcoreapp, unsafeType, module.TypeSystem);
 
-            module.Write(args[1]);
+            module.Write(args[2]);
         }
 
         // https://github.com/dotnet/corefx/blob/7942e7c3ed03cf7f19dffe539e23b84b4a85ad5a/src/System.Runtime.CompilerServices.Unsafe/src/System.Runtime.CompilerServices.Unsafe.il#L145
@@ -40,7 +42,7 @@ namespace Pidgin.DllRewriter
         }
 
         // https://github.com/dotnet/corefx/blob/7942e7c3ed03cf7f19dffe539e23b84b4a85ad5a/src/System.Runtime.CompilerServices.Unsafe/src/System.Runtime.CompilerServices.Unsafe.il#L262
-        private static void RewriteAsRefMethod(TypeDefinition unsafeType, TypeSystem typeSystem)
+        private static void RewriteAsRefMethod(bool netcoreapp, TypeDefinition unsafeType, TypeSystem typeSystem)
         {
             var asPointerMethod = unsafeType.Methods.Single(m => m.Name == "AsRef");
 
@@ -51,12 +53,21 @@ namespace Pidgin.DllRewriter
             }
 
             asPointerMethod.Body.MaxStackSize = 1;
-            asPointerMethod.Body.Variables.Clear();
-            asPointerMethod.Body.Variables.Add(new VariableDefinition(new ByReferenceType(typeSystem.Int32)));
-            ilProcessor.Emit(OpCodes.Ldarg_0);
-            ilProcessor.Emit(OpCodes.Stloc_0);
-            ilProcessor.Emit(OpCodes.Ldloc_0);
-            ilProcessor.Emit(OpCodes.Ret);
+
+            if (netcoreapp)
+            {
+                ilProcessor.Emit(OpCodes.Ldarg_0);
+                ilProcessor.Emit(OpCodes.Ret);
+            }
+            else
+            {
+                asPointerMethod.Body.Variables.Clear();
+                asPointerMethod.Body.Variables.Add(new VariableDefinition(new ByReferenceType(typeSystem.Int32)));
+                ilProcessor.Emit(OpCodes.Ldarg_0);
+                ilProcessor.Emit(OpCodes.Stloc_0);
+                ilProcessor.Emit(OpCodes.Ldloc_0);
+                ilProcessor.Emit(OpCodes.Ret);
+            }
         }
     }
 }
