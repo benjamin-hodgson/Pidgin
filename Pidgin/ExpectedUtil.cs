8ca8893c8e4eb8f8d9c223131ef79b037c291fb1
Benjamin Hodgson <bhodgson@stackoverflow.com>
2018-12-05 15:18:28 -0500

    Remove unused code

--- Pidgin/ExpectedUtil.cs
+++ Pidgin/ExpectedUtil.cs
@@ -6,17 +6,6 @@ namespace Pidgin
 {
     internal static class ExpectedUtil
     {
-        public static ImmutableSortedSet<T> Union<T>(params ImmutableSortedSet<T>[] input)
-            => Union(input.AsEnumerable());
-        public static ImmutableSortedSet<T> Union<T>(IEnumerable<ImmutableSortedSet<T>> input)
-        {
-            var builder = ImmutableSortedSet.CreateBuilder<T>();
-            foreach (var set in input)
-            {
-                builder.UnionWith(set);
-            }
-            return builder.ToImmutable();
-        }
         public static ImmutableSortedSet<T> Union<T>(ref PooledList<ImmutableSortedSet<T>> input)
         {
             var builder = ImmutableSortedSet.CreateBuilder<T>();
