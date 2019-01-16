e031a6f202b0fd718ecd0fbe9a8c47747ad37516
Benjamin Hodgson <bhodgson@stackoverflow.com>
2019-01-16 16:39:01 -0500

    add SourcePos asserts to tests

--- Pidgin.Tests/ParseStateTests.cs
+++ Pidgin.Tests/ParseStateTests.cs
@@ -10,8 +10,9 @@ namespace Pidgin.Tests
         public void TestEmptyInput()
         {
             var input = "".AsSpan();
-            var state = new ParseState<char>((_, x) => x, new SpanTokenStream<char>(ref input));
+            var state = new ParseState<char>((_, x) => x.IncrementCol(), new SpanTokenStream<char>(ref input));
 
+            Assert.Equal(new SourcePos(1, 1), state.SourcePos);
             Assert.False(state.HasCurrent);
         }
 
@@ -19,7 +20,7 @@ namespace Pidgin.Tests
         public void TestAdvance()
         {
             var input = "foo".AsSpan();
-            var state = new ParseState<char>((_, x) => x, new SpanTokenStream<char>(ref input));
+            var state = new ParseState<char>((_, x) => x.IncrementCol(), new SpanTokenStream<char>(ref input));
 
             Consume('f', ref state);
             Consume('o', ref state);
@@ -32,13 +33,10 @@ namespace Pidgin.Tests
         public void TestDiscardChunk()
         {
             var input = ('f' + new string('o', ChunkSize)).AsSpan();  // Length == ChunkSize + 1
-            var state = new ParseState<char>((_, x) => x, new SpanTokenStream<char>(ref input));
+            var state = new ParseState<char>((_, x) => x.IncrementCol(), new SpanTokenStream<char>(ref input));
 
             Consume('f', ref state);
-            for (var _ = 0; _ < ChunkSize; _++)
-            {
-                Consume('o', ref state);
-            }
+            Consume(new string('o', ChunkSize), ref state);
             Assert.False(state.HasCurrent);
         }
 
@@ -106,7 +104,7 @@ namespace Pidgin.Tests
         private static void AlignedChunkTest(int inputLength)
         {
             var input = ('f' + new string('o', inputLength - 1)).AsSpan();
-            var state = new ParseState<char>((_, x) => x, new SpanTokenStream<char>(ref input));
+            var state = new ParseState<char>((_, x) => x.IncrementCol(), new SpanTokenStream<char>(ref input));
 
             state.PushBookmark();
 
@@ -115,13 +113,14 @@ namespace Pidgin.Tests
             Assert.False(state.HasCurrent);
 
             state.Rewind();
+            Assert.Equal(new SourcePos(1, 1), state.SourcePos);
             Consume('f', ref state);
         }
 
         private static void UnalignedChunkTest(int inputLength)
         {
             var input = ("fa" + new string('o', inputLength - 2)).AsSpan();
-            var state = new ParseState<char>((_, x) => x, new SpanTokenStream<char>(ref input));
+            var state = new ParseState<char>((_, x) => x.IncrementCol(), new SpanTokenStream<char>(ref input));
 
             Consume('f', ref state);
 
@@ -130,20 +129,25 @@ namespace Pidgin.Tests
             Assert.False(state.HasCurrent);
 
             state.Rewind();
+            Assert.Equal(new SourcePos(1, 2), state.SourcePos);
             Consume('a', ref state);
         }
 
         private static void Consume(char expected, ref ParseState<char> state)
         {
+            var oldCol = state.SourcePos.Col;
             Assert.True(state.HasCurrent);
             Assert.Equal(expected, state.Current);
             state.Advance();
+            Assert.Equal(oldCol + 1, state.SourcePos.Col);
         }
 
         private static void Consume(string expected, ref ParseState<char> state)
         {
+            var oldCol = state.SourcePos.Col;
             AssertEqual(expected.AsSpan(), state.Peek(expected.Length));
             state.Advance(expected.Length);
+            Assert.Equal(oldCol + expected.Length, state.SourcePos.Col);
         }
 
         private static void AssertEqual(ReadOnlySpan<char> expected, ReadOnlySpan<char> actual)
