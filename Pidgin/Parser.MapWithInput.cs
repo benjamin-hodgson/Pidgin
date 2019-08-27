b428eee005d8ffa91df186bfa602d7ffcdc6eece
Benjamin Hodgson <bhodgson@stackoverflow.com>
2019-08-27 16:02:35 -0400

    docs

--- Pidgin/Parser.MapWithInput.cs
+++ Pidgin/Parser.MapWithInput.cs
@@ -4,6 +4,18 @@ namespace Pidgin
 {
     public abstract partial class Parser<TToken, T>
     {
+        /// <summary>
+        /// Returns a parser which runs the current parser and applies a selector function.
+        /// The selector function receives a <see cref="ReadOnlySpan{TToken}"/> as its first argument, and the result of the current parser as its second argument.
+        /// The <see cref="ReadOnlySpan{TToken}"/> represents the sequence of input tokens which were consumed by the parser.
+        /// </summary>
+        /// <param name="selector">
+        /// A selector function which computes a result of type <typeparamref name="U"/>.
+        /// The arguments of the selector function are a <see cref="ReadOnlySpan{TToken}"/> containing the sequence of input tokens which were consumed by this parser,
+        /// and the result of this parser.
+        /// </param>
+        /// <typeparam name="U">The result type</typeparam>
+        /// <returns>A parser which runs the current parser and applies a selector function.</returns>
         public Parser<TToken, U> MapWithInput<U>(ReadOnlySpanFunc<TToken, T, U> selector)
         {
             if (selector == null)
--- Pidgin/ReadOnlySpanFunc.cs
+++ Pidgin/ReadOnlySpanFunc.cs
@@ -2,5 +2,14 @@ using System;
 
 namespace Pidgin
 {
-    public delegate TReturn ReadOnlySpanFunc<TToken, in TParam, out TReturn>(ReadOnlySpan<TToken> span, TParam param);
+    /// <summary>
+    /// A function which computes a result from a <see cref="ReadOnlySpan{TToken}"/> and an additional argument.
+    /// </summary>
+    /// <param name="span">The input span</param>
+    /// <param name="param">An additional argument</param>
+    /// <typeparam name="T">The type of elements of the span</typeparam>
+    /// <typeparam name="TParam">The type of the additional argument</typeparam>
+    /// <typeparam name="TReturn">The type of the result computed by the function</typeparam>
+    /// <returns>The result</returns>
+    public delegate TReturn ReadOnlySpanFunc<T, in TParam, out TReturn>(ReadOnlySpan<T> span, TParam param);
 }
