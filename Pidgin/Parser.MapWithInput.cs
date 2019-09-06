446b2ec73832382b4da6a4ecf83eea5ac9fe6998
Benjamin Hodgson <bhodgson@stackoverflow.com>
2019-09-06 15:36:47 -0400

    add note on purpose

--- Pidgin/Parser.MapWithInput.cs
+++ Pidgin/Parser.MapWithInput.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 
 namespace Pidgin
 {
@@ -8,6 +9,8 @@ namespace Pidgin
         /// Returns a parser which runs the current parser and applies a selector function.
         /// The selector function receives a <see cref="ReadOnlySpan{TToken}"/> as its first argument, and the result of the current parser as its second argument.
         /// The <see cref="ReadOnlySpan{TToken}"/> represents the sequence of input tokens which were consumed by the parser.
+        /// 
+        /// This allows you to write "pattern"-style parsers which match a sequence of tokens and return a view of the part of the input stream which they matched.
         /// </summary>
         /// <param name="selector">
         /// A selector function which computes a result of type <typeparamref name="U"/>.
