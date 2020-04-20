1744dec766af437845147d10049ebf109c8ca438
Benjamin Hodgson <bhodgson@stackoverflow.com>
2020-04-20 16:27:18 -0400

    Add CurrentOffset

---
+++ Pidgin/Parser.CurrentOffset.cs
@@ -0,0 +1,18 @@
+namespace Pidgin
+{
+    public static partial class Parser<TToken>
+    {
+        /// <summary>
+        /// A parser which returns the number of input tokens which have been consumed.
+        /// </summary>
+        /// <returns>A parser which returns the number of input tokens which have been consumed</returns>
+        public static Parser<TToken, int> CurrentOffset { get; }
+            = new CurrentOffsetParser<TToken>();
+    }
+
+    internal sealed class CurrentOffsetParser<TToken> : Parser<TToken, int>
+    {
+        internal override InternalResult<int> Parse(ref ParseState<TToken> state)
+            => InternalResult.Success(state.Location, false);
+    }
+}
