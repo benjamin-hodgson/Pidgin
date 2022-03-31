4a9ab002de41c7be0ae5d0725d109a097c3f1d1a
GMIKE <roman.soloweow333@ya.ru>
2022-03-31 19:46:47 +0300

    Tests

---
+++ Pidgin.Tests/EnumParserTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace Pidgin.Tests;
+
+public class EnumParserTests
+{
+      internal enum TestEnum
+      {
+            Value1 = 0,
+            Value2,
+            Value3
+      }  
+      
+      [Fact]
+      public void EnumParseTest()
+      {
+        const string value1 = "value1";
+        var result = Parser.Enum<TestEnum>().Parse(value1);
+        Assert.False(result.Success);
+        
+        result = Parser.CIEnum<TestEnum>().Parse(value1);
+        Assert.Equal(TestEnum.Value1, result.Value);
+        
+        const string value2 = "Value2";
+        result = Parser.Enum<TestEnum>().Parse(value2);
+        Assert.Equal(TestEnum.Value2, result.Value);
+
+        result = Parser.CIEnum<TestEnum>().Parse(value2);
+        Assert.Equal(TestEnum.Value2, result.Value);
+
+      }
+}
