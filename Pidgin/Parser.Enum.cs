ae1153e6d0741da5f6499c4f3216792daca64e35
GMIKE <roman.soloweow333@ya.ru>
2022-04-14 02:21:28 +0300

    Enum parser, variable case sensitive string parser (#126)
    * Enum parser
    
    * API Consistency
    
    * Tests
    
    * Move enum tests to StringParserTests

---
+++ Pidgin/Parser.Enum.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pidgin
+{
+    public static partial class Parser
+    {
+        /// <summary>
+        /// Creates a parser that parses and returns one of enum values
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type</typeparam>
+        /// <returns>A parser that parses and returns one of enum values</returns>
+        public static Parser<char, TEnum> Enum<TEnum>()
+            where TEnum : struct, Enum
+        {
+            return OneOf(System.Enum.GetNames<TEnum>()
+                    .Select(String)
+                    .Select(Try))
+                .Select(System.Enum.Parse<TEnum>)
+                .Labelled("enum " + typeof(TEnum).Name);
+        }
+        
+        /// <summary>
+        /// Creates a parser that parses and returns one of enum values, in a case insensitive manner.
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type</typeparam>
+        /// <returns>A parser that parses and returns one of enum values</returns>
+        public static Parser<char, TEnum> CIEnum<TEnum>()
+            where TEnum : struct, Enum
+        {
+            return OneOf(System.Enum.GetNames<TEnum>()
+                    .Select(CIString)
+                    .Select(Try))
+                .Select(x=> System.Enum.Parse<TEnum>(x, true))
+                .Labelled("enum " + typeof(TEnum).Name);
+        }
+        
+    }
+}
