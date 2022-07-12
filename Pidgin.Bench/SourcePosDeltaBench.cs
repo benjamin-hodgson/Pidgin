647c03bd663f0015c42464e58a5519dfa866b951
Benjamin Hodgson <3857587+benjamin-hodgson@users.noreply.github.com>
2022-07-12 15:59:39 +0000

    avx2 version

--- Pidgin.Bench/SourcePosDeltaBench.cs
+++ Pidgin.Bench/SourcePosDeltaBench.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 using BenchmarkDotNet.Attributes;
 
 namespace Pidgin.Bench;
 
-
 public class SourcePosDeltaBench
 {
+    private const ulong _linesMask = 0xFFFFFFFF00000000;
+    private const ulong _colsMask = 0x00000000FFFFFFFF;
     private SourcePosDelta1[]? _data1;
     private SourcePosDelta2[]? _data2;
 
@@ -71,6 +74,35 @@ public class SourcePosDeltaBench
         }
     }
 
+    [Benchmark]
+    public unsafe void Simd()
+    {
+        var sum = Vector256<ulong>.Zero;
+        fixed (SourcePosDelta2* ptr = _data2)
+        {
+            for (var i = 0; i < _data2!.Length / 16; i++)
+            {
+                for (var offset = 0L; offset < 4; offset++)
+                {
+                    var ix = Avx2.Add(Vector256.Create(0, 4, 8, 12), Vector256.Create(offset));
+                    var data = Avx2.GatherVector256((ulong*)(ptr + i * 16), ix, 1);
+                    var mask = Avx2.BlendVariable(
+                        Vector256.AsByte(Vector256.Create(-1L)),
+                        Vector256.AsByte(Vector256.Create(_linesMask)),
+                        Vector256.AsByte(Avx2.CompareEqual(Avx2.And(data, Vector256.Create(_linesMask)), Vector256<ulong>.Zero))
+                    );
+                    sum = Avx2.Add(Avx2.And(sum, Vector256.AsUInt64(mask)), data);
+                }
+            }
+        }
+        var result = new SourcePosDelta2(0);
+        var output = stackalloc SourcePosDelta2[4];
+        Avx.Store((ulong*)output, sum);
+        for (var i = 0; i < 4; i++)
+        {
+            result = result.PlusBenjamin(*(output + i));
+        }
+    }
 
     private struct SourcePosDelta1
     {
@@ -93,8 +125,6 @@ public class SourcePosDeltaBench
 
     private struct SourcePosDelta2
     {
-        private const ulong _linesMask = 0xFFFFFFFF00000000;
-        private const ulong _colsMask = 0x00000000FFFFFFFF;
 
         public ulong Data;
 
